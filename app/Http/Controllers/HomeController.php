fa2349d671ab662c1a426d069582abf2a4f8ce08
farukwebcode21 <farukahmadmk@yahoo.com>
2023-06-23 18:18:54 +0600

    complete all dummy data

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function page(Request $request){
+
+    }
+    public function heroData(Request $request){
+        
+    }
+    public function aboutData(Request $request){
+        
+    }
+    public function socialData(Request $request){
+        
+    }
+}
